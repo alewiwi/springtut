5146587bf25ba1358c4036661efc390ca53849e7
mahmoud <mewiwi@gmail.com>
2020-05-14 04:28:02 +0300

    JPA issue is fixed

--- src/main/java/com/mercury/cms/CmsApplication.java
+++ src/main/java/com/mercury/cms/CmsApplication.java
@@ -2,8 +2,10 @@ package com.mercury.cms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.mercury.model")
 public class CmsApplication {
 
 	public static void main(String[] args) {
